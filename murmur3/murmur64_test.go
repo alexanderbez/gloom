eaa3feac721fbe5e0fdccd2dabed1a091ecfb68e
Aleksandr Bezobchuk <abezobchuk@gmail.com>
2018-06-12 13:00:51 -0400

    Update murmur3 unit tests

--- murmur3/murmur64_test.go
+++ murmur3/murmur64_test.go
@@ -2,55 +2,55 @@ package murmur3
 
 import "testing"
 
-func TestSum128(t *testing.T) {
+func TestSum64(t *testing.T) {
 	testCases := []struct {
 		seed uint64
 		data []byte
-		hash [2]uint64
+		hash uint64
 	}{
 		{
 			data: nil,
-			hash: [2]uint64{0x0, 0x0},
+			hash: 0x0,
 		},
 		{
 			data: []byte{},
-			hash: [2]uint64{0x0, 0x0},
+			hash: 0x0,
 		},
 		{
 			data: []byte("Hello, World!!!!"),
-			hash: [2]uint64{0xb57df55e4edee585, 0x7fbabd101b969fb2},
+			hash: 0xb57df55e4edee585,
 		},
 		{
 			seed: 0xfa,
 			data: []byte("Hello, World!!!!"),
-			hash: [2]uint64{0xb02fba6b1d629cae, 0xab907abfb23bfcfc},
+			hash: 0xb02fba6b1d629cae,
 		},
 		{
 			data: []byte("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa."),
-			hash: [2]uint64{0xaf11090ad904f11a, 0x52b5309456f0ad38},
+			hash: 0xaf11090ad904f11a,
 		},
 		{
 			seed: 0xfa,
 			data: []byte("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa."),
-			hash: [2]uint64{0x2d36aa481cf715ec, 0x15f04536764cf671},
+			hash: 0x2d36aa481cf715ec,
 		},
 		{
 			data: []byte("@@@##!&^#%$!+_][;//"),
-			hash: [2]uint64{0xae66907cdc6d6934, 0xf8e73c715a15b592},
+			hash: 0xae66907cdc6d6934,
 		},
 		{
 			seed: 0xfa,
 			data: []byte("@@@##!&^#%$!+_][;//"),
-			hash: [2]uint64{0x9ec3da2f2c2441b5, 0xfe8d97a6d3920f83},
+			hash: 0x9ec3da2f2c2441b5,
 		},
 	}
 
 	for _, tc := range testCases {
-		s := New128WithSeed(tc.seed)
+		s := New64WithSeed(tc.seed)
 		s.Write(tc.data)
 
-		if s.Sum128() != tc.hash {
-			t.Errorf("unexpected result: expected: %v, got: %v", tc.hash, s.Sum128())
+		if s.Sum64() != tc.hash {
+			t.Errorf("unexpected result: expected: %v, got: %v", tc.hash, s.Sum64())
 		}
 	}
 }
